1096a9be545d6ab8f392ed830452405ad0fe3aed
Sofia M M <98534255+SofiaFasullo@users.noreply.github.com>
2022-02-10 14:36:14 +0000

    turtle

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-mylist = (input("Enter a number"))
-mylist = (input("Enter a second number"))
-mylist = (input("Enter a third number"))
-mylist = (input("Enter a fourth number"))
+
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("purple")
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+
+Window = turtle.Screen()
+Window.bgcolor("orange")
+Window.exitonclick()
